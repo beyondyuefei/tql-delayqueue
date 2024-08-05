4539a746ba799baef91710de34a813e74a747da8
李强 <liqiang@gram-tech.com>
2024-08-05 10:44:38 +0800

    make AbstractPartitionAssignerService#initWorkersPartitionMap final

--- core/src/main/java/tql/delayqueue/partition/assigner/AbstractPartitionAssignerService.java
+++ core/src/main/java/tql/delayqueue/partition/assigner/AbstractPartitionAssignerService.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 public abstract class AbstractPartitionAssignerService implements PartitionAssignerService{
-    protected Map<String, List<Integer>> initWorkersPartitionMap(final List<PartitionWorker> workers) {
+    protected final Map<String, List<Integer>> initWorkersPartitionMap(final List<PartitionWorker> workers) {
         final Map<String, List<Integer>> workersPartitionMap = new HashMap<>();
         workers.forEach(partitionWorker -> workersPartitionMap.put(partitionWorker.getWorkerUniqueIdentifier(), new ArrayList<>()));
         return workersPartitionMap;
